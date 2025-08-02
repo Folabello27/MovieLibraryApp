294cd49d405a895dbfd4eed3077bfb41ac693f0a
Raheem Folarin <Folabello27@gmail.com>
2025-08-02 15:36:17 +0100

    feat: add layered project structure

---
+++ MovieLibrary.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace MovieLibrary.Domain;
+
+public class Class1
+{
+
+}
---
+++ MovieLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MovieLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
